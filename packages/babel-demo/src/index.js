ee1927832f365a0eeed3aa75c4d1667f6b823402
GangYangZhao <zhaogangyang@ones.ai>
2022-02-18 17:43:22 +0800

    feat: 添加react redux相关依赖

--- packages/babel-demo/src/index.js
+++ packages/babel-demo/src/index.js
@@ -1,12 +1,2 @@
-import './a.js';
-import './style.css';
-/**
- * test
- */
-const a = 1;
-
-const fn = () => 1;
-
-// const weakMap = new weakMap();
-
-Promise.resolve().finally();
+import React from 'react';
+import ReactDOM from 'react-dom';
